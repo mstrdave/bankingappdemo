fb095b7d971e8201a9e174934c65a924ac8ba6ad
mstrdave <mstrdave04@gmail.com>
2024-07-26 19:35:09 -0500

    first commit

---
+++ app/layout.tsx
@@ -0,0 +1,31 @@
+import type { Metadata } from "next";
+import { Inter, IBM_Plex_Serif } from "next/font/google";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"], variable: '--font-inter'});
+const ibmPlexSerif = IBM_Plex_Serif({
+  subsets: ['latin'],
+  weight:['400', '700'],
+  variable: '--font-ibm-plex-sarif'
+
+})
+
+export const metadata: Metadata = {
+  title: "Create Next App",
+  description: "Valted is a new banking platform built to grow with you",
+  icons:{
+    icon: '/icon/logo.svg'
+  }
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body className={`${inter.variable} ${ibmPlexSerif.variable}`}>{children}</body>
+    </html>
+  );
+}
